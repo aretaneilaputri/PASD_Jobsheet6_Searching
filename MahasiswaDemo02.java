f075695191d04d20cba08d234d818ab4e644a46b
“aretaneilaputri <retaaneila@gmail.com>
2026-04-08 09:38:58 +0700

    6.3 - Percobaan Binary Search (MahasiswaDemo02.java)

--- MahasiswaDemo02.java
+++ MahasiswaDemo02.java
@@ -38,6 +38,22 @@ public class MahasiswaDemo02 {
         list.tampilPosisi(cari, pss);
         list.tampilDataSearch(cari, pss);
 
+        // lakukan pencarian data binary
+        System.out.println("--------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("--------------------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari2 = sc.nextDouble();
+
+        System.out.println("--------------------------------------------");
+        System.out.println("menggunakan binary search");
+        System.out.println("--------------------------------------------");
+        double posisi2 = list.findBinarySearch(cari2, 0, jumMhs - 1);
+        int pss2 = (int) posisi2;
+        list.tampilPosisi(cari2, pss2);
+        list.tampilDataSearch(cari2, pss2);
+
         sc.close();
     }
 }
